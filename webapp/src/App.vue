cc6ce8712867f728f8dc5c0a226843e6bc710862
zhangchunzhong <zczxyz@126.com>
2023-09-01 20:39:08 +0800

    create by goxygen

---
+++ webapp/src/App.vue
@@ -0,0 +1,67 @@
+<script setup>
+import TechItems from './components/TechItems.vue'
+import logoSVG from '@/assets/logo.svg';
+</script>
+
+<template>
+  <main id="app">
+    <h2 class="title">vue-admin</h2>
+    <div class="logo">
+      <img :src="logoSVG" height="150" alt="logo" />
+    </div>
+    <div>
+      This project is generated with
+      <b>
+        <a href="https://github.com/shpota/goxygen">goxygen</a>
+      </b>.
+      <p />The following list of technologies comes from
+      a REST API call to the Go-based back end. Find
+      and change the corresponding code in
+      <code>webapp/src/components/TechItems.vue</code>
+      and <code>server/web/app.go</code>.
+      <TechItems />
+    </div>
+  </main>
+</template>
+
+<style>
+body {
+  margin-top: 5%;
+  padding-right: 5%;
+  padding-left: 5%;
+  font-size: larger;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+@media screen and (min-width: 800px) {
+  body {
+    padding-right: 15%;
+    padding-left: 15%;
+  }
+}
+
+@media screen and (min-width: 1600px) {
+  body {
+    padding-right: 30%;
+    padding-left: 30%;
+  }
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+    monospace;
+  background-color: #b3e6ff;
+}
+
+.title {
+  text-align: center;
+}
+
+.logo {
+  text-align: center;
+}
+</style>
---
+++ webapp/src/components/TechItems.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul class="technologies">
+    <li v-for="technology in technologies" v-bind:key="technology.name">
+      <b>{{technology.name}}</b>: {{technology.details}}
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'TechItems',
+  data() {
+    return {
+      technologies: []
+    };
+  },
+  mounted() {
+    axios
+      .get(`${import.meta.env.VITE_API_URL}/api/technologies`)
+      .then(response => (this.technologies = response.data));
+  }
+};
+</script>
+
+<style scoped>
+.technologies {
+  margin-top: 5px;
+}
+</style>
